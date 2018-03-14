0646a3d7cca26d56d33d8e11cf00577c56f2bfe4
Kevin de Roulet <kderoulet@yahoo.com>
2018-03-13 20:47:34 -0700

    ex 43-44

--- ex43.py
+++ ex43.py
@@ -6,7 +6,7 @@ class Scene(object):
 
     def enter(self):
         print("This scene is not yet configured.")
-        print("Sublass it and implement enter().")
+        print("Subclass it and implement enter().")
         exit(1)
 
 class Engine(object):
@@ -35,62 +35,67 @@ class Death(Scene):
         "You're worse than your Dad's jokes."
     ]
 
+    def enter(self):
+        print(Death.quips[randint(0, len(self.quips)-1)])
+        exit(1)
+
 class CentralCorridor(Scene):
-    print(dedent("""
-        The Gothons of Planet Percal #25 have invaded your ship and 
-        destroyed your entire crew. You are the last surviving 
-        member and your last mission is to get the neutron destruct
-        bomb from the Wapons Armory, put it in the bridge, and
-        blow the ship up after getting into an escape pod.
+    def enter(self):
+        print(dedent("""
+            The Gothons of Planet Percal #25 have invaded your ship and 
+            destroyed your entire crew. You are the last surviving 
+            member and your last mission is to get the neutron destruct
+            bomb from the Wapons Armory, put it in the bridge, and
+            blow the ship up after getting into an escape pod.
+            
+            You're running down the central corridor to the Weapons
+            Armory when a Gothon jumps out, red scaly skin, dark grimy
+            teeth, and evil clown costume flowing around his hate
+            filled body. He's blocking the door to the Armory and
+            about to pull a weapon to blast you.
+            """))
         
-        You're running down the central corridor to the Weapons
-        Armory when a Gothon jumps out, red scaly skin, dark grimy
-        teeth, and evil clown costume flowing around his hate
-        filled body. He's blocking the door to the Armory and
-        about to pull a weapon to blast you.
-        """))
-    
-    action = input("> ")
+        action = input("> ")
 
-    if action == "shoot!":
-        print(dedent("""
-            Quick on the draw you yank out your blaster and fire
-            it at the Gothon. His clown costume is flowing and 
-            moving around his body, which throws off your aim.
-            Your laser hits his cosume but misses him entirely.
-            This completely ruins his brand new costume his mother
-            bought him, which makes him fly into an insane rage
-            and blasts you repeatedly in the face until you are
-            dead. Then he eats you.
-            """))
-        return 'death'
+        if action == "shoot!":
+            print(dedent("""
+                Quick on the draw you yank out your blaster and fire
+                it at the Gothon. His clown costume is flowing and 
+                moving around his body, which throws off your aim.
+                Your laser hits his cosume but misses him entirely.
+                This completely ruins his brand new costume his mother
+                bought him, which makes him fly into an insane rage
+                and blasts you repeatedly in the face until you are
+                dead. Then he eats you.
+                """))
+            return 'death'
 
-    elif action == "dodge!":
-        print(dedent("""
-            Like a world class boxer you dodge, weave, slip and
-            slide right as the Gothon's blaster cranks a laser
-            past your head. In the middle of your artful dodge
-            your foot slips and you bang your head on the metal 
-            wall and pass out. You wake up shortly after only to
-            die as the Gothon stomps on your head and eats you.
-            """))
-        return 'death'
+        elif action == "dodge!":
+            print(dedent("""
+                Like a world class boxer you dodge, weave, slip and
+                slide right as the Gothon's blaster cranks a laser
+                past your head. In the middle of your artful dodge
+                your foot slips and you bang your head on the metal 
+                wall and pass out. You wake up shortly after only to
+                die as the Gothon stomps on your head and eats you.
+                """))
+            return 'death'
 
-    elif action == "tell a joke":
-        print(dedent("""
-            Lucky for you they made you learn Gothon insults in 
-            the academy. You tell the one Gothon joke you know:
-            Lbhe zbgure vf fb sng, jura fur fvgf nebhaq gur ubhfr,
-            fur fgvf nebhaq gur ubhfr. The Gothon stops, tries
-            not to laugh, then busts out laughing and can't move.
-            While he's laughing you run up and shoot him square in
-            the head putting him down, then jump through the
-            Weapon Armory Room.
-            """))
-        return 'laser_weapon_armory'
-    else:
-        print("DOES NOT COMPUTE!")
-        return 'cetral_corridor'
+        elif action == "tell a joke":
+            print(dedent("""
+                Lucky for you they made you learn Gothon insults in 
+                the academy. You tell the one Gothon joke you know:
+                Lbhe zbgure vf fb sng, jura fur fvgf nebhaq gur ubhfr,
+                fur fgvf nebhaq gur ubhfr. The Gothon stops, tries
+                not to laugh, then busts out laughing and can't move.
+                While he's laughing you run up and shoot him square in
+                the head putting him down, then jump through the
+                Weapon Armory Room.
+                """))
+            return 'laser_weapon_armory'
+        else:
+            print("DOES NOT COMPUTE!")
+            return 'cetral_corridor'
 
 class LaserWeaponArmory(Scene):
 
@@ -238,5 +243,5 @@ class Map(object):
         return self.next_scene(self.start_scene)
 
 a_map = Map('central_corridor')
-a_game = Enginer('a_map')
+a_game = Engine(a_map)
 a_game.play()
