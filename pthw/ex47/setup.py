e09b9b4fdc45061af4eef80af6d1e60cf85c8c09
Kevin de Roulet <kderoulet@yahoo.com>
2018-03-15 09:09:38 -0700

    ex 47

---
+++ pthw/ex47/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setuptools
+except:
+    from distutils.core import setup
+
+config = {
+    'description': 'My Project',
+    'author': 'Kevin de Roulet',
+    'url': 'url',
+    'download_url': 'dlurl',
+    'author_email': 'kevinderoulet@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'ex47'
+}
+
+setup(**config)
